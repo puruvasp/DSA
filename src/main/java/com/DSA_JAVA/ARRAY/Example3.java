ae868155f154a7c4f26aa2c595b00efabb5b332a
puruv <puruvas0954@gmail.com>
2025-11-06 14:54:41 +0530

    adding new to array

---
+++ src/main/java/com/DSA_JAVA/ARRAY/Example3.java
@@ -0,0 +1,23 @@
+package com.DSA_JAVA.ARRAY;
+
+public class Example3 {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+                start++;
+                end--;
+            }
+        System.out.print("Reversed Array: ");
+        for (int i = 0; i < arr.length; i++)
+        {System.out.print(arr[i] + " ");}
+    }
+}
